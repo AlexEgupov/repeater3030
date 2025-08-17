c4df59e0825cbde4c7f2cffc030fd16d3aa23e30
lexonz <lexonz@inbox.ru>
2025-08-17 11:42:04 +0300

    Start

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var myVar = 'ABOBA';
-
-const firstFunc = (m) => {
-    alert(m);
-
-}
-
-firstFunc(myVar);
+const title = myName;
+const screens = 0;
+const screenPrice = 0;
+const rollback = 0;
+let fullPrice = 0;
+const adaptive = true;

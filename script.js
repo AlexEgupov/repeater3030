9311370ea8b1f7b4559fa7a168d1d0dfafe3222e
lexonz <lexonz@inbox.ru>
2025-08-19 19:51:57 +0300

    completed lesson03 about type of data

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const title = '';
-const screens = 0;
-const screePrice = 0;
-const rollback = 0;
-let fullPrice = 0;
+const title = 'Another try';
+const screens = "Простые, Сложные, Интерактивные";
+const screePrice = 10000;
+const rollback = 10;
+let fullPrice = 100000;
 const adaptive = true;
 
-alert('1213');
-console.log('2321321');
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screePrice + " рублей/ долларов/гривен/юани");
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
+console.log(screens.toLowerCase().split(', '));
+console.log('Процент отката посреднику за работу ' + fullPrice * (rollback / 100));
